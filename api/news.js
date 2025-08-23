3fe2c347cf216a5933b272543e8910bcc6a808b0
muralikrishnaponugoti <ponugotimurali26@gmail.com>
2025-08-23 18:39:07 +0530

    resolved api issue

--- api/news.js
+++ api/news.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   try {
-    const response = await fetch('../api/news');
+    const response = await fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=5dc97cf8cdeb49d0b1152a08c8558652');
     const data = await response.json();
     res.status(200).json(data);
   } catch (error) {

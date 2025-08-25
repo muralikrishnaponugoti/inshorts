32ed6bc853b6b04cb5139e1d0108bdee1c5e4b51
murali krishna ponugoti <155852481+muralikrishnaponugoti@users.noreply.github.com>
2025-08-25 17:19:35 +0530

    Update news.js

--- api/news.js
+++ api/news.js
@@ -3,18 +3,14 @@ export default async function handler(req, res) {
   let url=''
   category=category.trim();
   if(category!=='undefined' && category!=='india'){
-    console.log('called in category',category);
-    console.log('print in newsjs',typeof category);
-    url=`https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=654765756`;
+    url=`https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=5dc97cf8cdeb49d0b1152a08c8558652`;
   }
   else{
-    console.log('called in general',category);
-    url=`https://newsapi.org/v2/top-headlines?country=us&apiKey=756456465`;
+    url=`https://newsapi.org/v2/top-headlines?country=us&apiKey=5dc97cf8cdeb49d0b1152a08c8558652`;
   }
   try {
     const response = await fetch(url);
     const data = await response.json();
-    console.log('calling in api',data);
     res.status(200).json(data);
   } catch (error) {
     res.status(500).json({ error: "Failed to fetch news" });

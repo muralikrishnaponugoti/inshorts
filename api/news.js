9fbdde36329f39b46d6f6d6259a5ef33c18921b0
murali krishna ponugoti <155852481+muralikrishnaponugoti@users.noreply.github.com>
2025-08-25 16:35:03 +0530

    Update news.js

--- api/news.js
+++ api/news.js
@@ -1,13 +1,13 @@
 export default async function handler(req, res) {
   const {category,countary}=req.query;
   let url=''
-  if(category&& category!=='india'){
+  if(category!==undefined &&category!=='india'){
     console.log('called in category',category);
     url=`https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=5dc97cf8cdeb49d0b1152a08c8558652`;
   }
   else{
-    url=`https://newsapi.org/v2/top-headlines?country=us&apiKey=5dc97cf8cdeb49d0b1152a08c8558652`;
     console.log('called in general',category);
+    url=`https://newsapi.org/v2/top-headlines?country=us&apiKey=5dc97cf8cdeb49d0b1152a08c8558652`;
   }
   try {
     const response = await fetch(url);
